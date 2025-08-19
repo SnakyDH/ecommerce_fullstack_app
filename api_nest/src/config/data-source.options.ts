519e8373f63fa10e192407b4269c21ada3f28488
SnakyDH <skdanielg5@gmail.com>
2025-08-19 10:56:36 -0500

    feat(db): config postgres db

---
+++ api_nest/src/config/data-source.options.ts
@@ -0,0 +1,24 @@
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+dotenv.config();
+
+export const typeOrmConfig: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_DATABASE || 'ecommerce_fullstack_app_coffee',
+  entities: [path.join(__dirname, '../../**/*.entity{.ts,.js}')],
+  migrations: [
+    path.join(__dirname, '../db/migrations/*.ts'),
+    path.join(__dirname, '../db/seeds/*.ts'),
+  ],
+  synchronize: false,
+  logging: true,
+};
+
+const appDataSource = new DataSource(typeOrmConfig);
+export default appDataSource;
